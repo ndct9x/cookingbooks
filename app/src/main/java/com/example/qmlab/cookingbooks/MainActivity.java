316ccb827df9e5e31291dce1a1941beadae331d5
quannv <nvquan2804@gmail.com>
2018-12-05 20:01:03 +0700

    slide pages

--- app/src/main/java/com/example/qmlab/cookingbooks/MainActivity.java
+++ app/src/main/java/com/example/qmlab/cookingbooks/MainActivity.java
@@ -1,30 +1,74 @@
 package com.example.qmlab.cookingbooks;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements Tab1.OnFragmentInteractionListener,Tab2.OnFragmentInteractionListener,Tab3.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         /*requestWindowFeature(Window.FEATURE_NO_TITLE);*/
 
-        /*this.getSupportActionBar().hide();*/
+        this.getSupportActionBar().hide();
 
         setContentView(R.layout.activity_main);
 
+        //
+
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tablayout);
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
+        //tabLayout.addTab(tabLayout.newTab().setText("Tab 0"));
+
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager = (ViewPager)findViewById(R.id.pager);
+        final PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.setOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+
+        //
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+        //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Tab1()).commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 
     @Override
@@ -49,17 +93,20 @@ public class MainActivity extends AppCompatActivity{
 
             switch (menuItem.getItemId()){
                 case R.id.nav_home:
-                    selectedFragment = new HomeFragment();
+                    selectedFragment = new Tab1();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
                     break;
                 case R.id.nav_favorites:
                     selectedFragment = new FavoritesFragment();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
                     break;
                 case R.id.nav_user:
                     selectedFragment = new UserFragment();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
                     break;
             }
 
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
 
             return true;
         }
---
+++ app/src/main/java/com/example/qmlab/cookingbooks/PagerAdapter.java
@@ -0,0 +1,40 @@
+package com.example.qmlab.cookingbooks;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+
+    int mNoOfTabs;
+
+    public  PagerAdapter(FragmentManager fm, int NumberOfTabs){
+        super(fm);
+        this.mNoOfTabs = NumberOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position){
+        switch (position){
+            case 0:
+                Tab1 tab1 = new Tab1();
+                return tab1;
+            case 1:
+                Tab2 tab2 = new Tab2();
+                return tab2;
+            case 2:
+                Tab3 tab3 = new Tab3();
+                return tab3;
+           /* case 3:
+                Tab4 tab0 = new Tab4();
+                return tab0;*/
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount(){
+        return mNoOfTabs;
+    }
+}
