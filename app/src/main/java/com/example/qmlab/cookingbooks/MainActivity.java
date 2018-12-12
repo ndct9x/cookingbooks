386f36e9af6f2096217f9a6ce8c8a6fc99946c20
quannv <nvquan2804@gmail.com>
2018-12-12 19:35:34 +0700

    update menu action bar

--- app/src/main/java/com/example/qmlab/cookingbooks/MainActivity.java
+++ app/src/main/java/com/example/qmlab/cookingbooks/MainActivity.java
@@ -2,45 +2,59 @@ package com.example.qmlab.cookingbooks;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.Window;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements Tab1.OnFragmentInteractionListener,Tab2.OnFragmentInteractionListener,Tab3.OnFragmentInteractionListener{
+public class MainActivity extends AppCompatActivity implements Tab1.OnFragmentInteractionListener,Tab2.OnFragmentInteractionListener,Tab3.OnFragmentInteractionListener, Tab4.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         /*requestWindowFeature(Window.FEATURE_NO_TITLE);*/
 
-        this.getSupportActionBar().hide();
+        /*this.getSupportActionBar().hide();*/
 
         setContentView(R.layout.activity_main);
 
         //
 
         TabLayout tabLayout = (TabLayout)findViewById(R.id.tablayout);
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_menu_black_24dp));
         tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
         tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
         tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
         tabLayout.addTab(tabLayout.newTab().setText("Tab 4"));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_search_white_24dp));
 
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        tabLayout.getTabAt(1).select();
 
         final ViewPager viewPager = (ViewPager)findViewById(R.id.pager);
         final PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
         viewPager.setAdapter(adapter);
-        viewPager.setOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
 
-        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        viewPager.setCurrentItem(1, false);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 viewPager.setCurrentItem(tab.getPosition());
@@ -58,12 +72,18 @@ public class MainActivity extends AppCompatActivity implements Tab1.OnFragmentIn
         });
 
 
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setting the title
+        //toolbar.setTitle("My Toolbar");
 
+        //placing toolbar in place of actionbar
+        //setSupportActionBar(toolbar);
         //
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
 
-        //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Tab1()).commit();
+        //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Tab2()).commit();
+
     }
 
     @Override
@@ -71,16 +91,17 @@ public class MainActivity extends AppCompatActivity implements Tab1.OnFragmentIn
 
     }
 
-    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_actions, menu);
-        return super.onCreateOptionsMenu(menu);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_actions, menu);
+        return true;
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.search:
-                return true;
+            case R.id.about:
+                Toast.makeText(this, "You clicked about", Toast.LENGTH_SHORT).show();
+                break;
         }
 
         return super.onOptionsItemSelected(item);
@@ -94,7 +115,11 @@ public class MainActivity extends AppCompatActivity implements Tab1.OnFragmentIn
             switch (menuItem.getItemId()){
                 case R.id.nav_home:
                     selectedFragment = new Tab1();
+                    FragmentTransaction assss =  getSupportFragmentManager().beginTransaction();
+                    assss.hide(selectedFragment);
+                    assss.commit();
                     getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
                     break;
                 case R.id.nav_favorites:
                     selectedFragment = new FavoritesFragment();
